ed1051e2719a8503315ec94ce8703e0bf3916356
Sheamus Clifford <59657367+SCLIFFORD78@users.noreply.github.com>
2022-10-08 11:24:32 +0100

    Basic Svelte App

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<div class="uk-container">
+  <div class="uk-flex uk-flex-center uk-flex-middle uk-margin">
+    <div class="uk-width-2-3@m uk-card uk-card-default uk-padding-small uk-text-center">
+      <div class="title"> Simple App</div>
+      <div class="uk-text-muted uk-text-small">Ready to Build</div>
+    </div>
+  </div>
+</div>
