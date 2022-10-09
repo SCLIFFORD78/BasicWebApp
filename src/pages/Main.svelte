78d110b16facad177bbdccb275e1f943051bbe28
Sheamus Clifford <59657367+SCLIFFORD78@users.noreply.github.com>
2022-10-09 14:14:08 +0100

    Made Table for list main pg

--- src/pages/Main.svelte
+++ src/pages/Main.svelte
@@ -4,14 +4,12 @@
 
   onMount(async () => {
     await fetch(`http://localhost:4000/api/candidates`)
-        .then(r => r.json())
-        .then(data => {
-          console.log(data)
-          candidates = data;
-        });
-});
-
-
+      .then((r) => r.json())
+      .then((data) => {
+        console.log(data);
+        candidates = data;
+      });
+  });
 </script>
 
 <div class="uk-container">
@@ -19,19 +17,27 @@
     <div
       class="uk-width-2-3@m uk-card uk-card-default uk-padding-small uk-text-center"
     >
-      <div class="title">Simple Todo List</div>
+      <div class="title">Simple MongoDB List</div>
       <div class="uk-text-muted uk-text-small">Fun things to do</div>
       <a href="/#/page2">Page 2</a>
-      {#if candidates}
-      {#each candidates as candidate}
-        <tr>
-          <td>{candidate.firstName}</td>
-          <td>{candidate.lastName}</td>
-          <td> {candidate._id} </td>
-          <td> {candidate.office} </td>
-        </tr>
-      {/each}
-    {/if}
+      <table class="uk-table">
+        {#if candidates}
+          <thead>
+            <th> First Name </th>
+            <th> Last Name </th>
+            <th> ID </th>
+            <th> Office </th>
+          </thead>
+          {#each candidates as candidate}
+            <tr class="uk-text-left">
+              <td>{candidate.firstName}</td>
+              <td>{candidate.lastName}</td>
+              <td><a href="/#/page2">{candidate._id}</a></td>
+              <td>{candidate.office}</td>
+            </tr>
+          {/each}
+        {/if}
+      </table>
     </div>
   </div>
 </div>
